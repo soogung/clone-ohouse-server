e865e5c5c9f75a6e67650bdb4b9c6a32c6c8b713
gimhanul <gimhanuly@gmail.com>
2022-08-26 14:45:21 +0900

    init

---
+++ src/main/kotlin/com/soogung/ohouse/OhouseApplication.kt
@@ -0,0 +1,11 @@
+package com.soogung.ohouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OhouseApplication
+
+fun main(args: Array<String>) {
+	runApplication<OhouseApplication>(*args)
+}
---
+++ src/test/kotlin/com/soogung/ohouse/OhouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.soogung.ohouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OhouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
