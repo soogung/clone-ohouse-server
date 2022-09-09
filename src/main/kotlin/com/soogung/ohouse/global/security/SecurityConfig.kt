acb63d0a36d3d4195b15596786e0300711799f5f
gimhanul <gimhanuly@gmail.com>
2022-09-09 22:56:12 +0900

    ADD :: security config (#3)
    - security config 추가
    - 원래는 클래스 구현해서 해야 하는데 쓰지말라해서 새로운 방식으로 함 ㅎㅎ

---
+++ src/main/kotlin/com/soogung/ohouse/global/security/SecurityConfig.kt
@@ -0,0 +1,63 @@
+package com.soogung.ohouse.global.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.soogung.ohouse.global.security.auth.AuthDetailsService
+import com.soogung.ohouse.global.security.jwt.JwtTokenProvider
+import com.soogung.ohouse.global.security.jwt.filter.JwtAuthenticationFilter
+import com.soogung.ohouse.global.security.jwt.filter.JwtExceptionFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig(
+    private val authDetailsService: AuthDetailsService,
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val mapper: ObjectMapper,
+) {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder();
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web
+                .ignoring()
+                .mvcMatchers("/swagger-ui.html/**", "/configuration/**", "/swagger-resources/**", "/v2/api-docs","/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}")
+        }
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
+        http
+            .httpBasic().disable()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .formLogin().disable();
+
+        http
+            .addFilterBefore(JwtAuthenticationFilter(authDetailsService, jwtTokenProvider), UsernamePasswordAuthenticationFilter::class.java)
+            .addFilterBefore(JwtExceptionFilter(mapper), JwtAuthenticationFilter::class.java)
+
+        return http.build()
+    }
+}
