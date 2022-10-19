4a5d39073fc99f509db1b4dacec9a79c01c383c7
gimhanul <gimhanuly@gmail.com>
2022-10-19 17:41:06 +0900

    REFACTOR :: user query (#14)

--- src/main/kotlin/com/soogung/ohouse/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/soogung/ohouse/domain/user/domain/repository/UserRepository.kt
@@ -2,7 +2,10 @@ package com.soogung.ohouse.domain.user.domain.repository
 
 import com.soogung.ohouse.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.transaction.annotation.Transactional
 
 interface UserRepository: JpaRepository<User, Long> {
 
@@ -14,4 +17,9 @@ interface UserRepository: JpaRepository<User, Long> {
 
     @Query("SELECT count(u) > 0 FROM User u WHERE u.email = :email")
     fun existsUserByEmail(email: String): Boolean
+
+    @Modifying
+    @Transactional
+    @Query("INSERT INTO tbl_user (email, password, nickname, grade) VALUES (:#{#u.email},:#{#u.password},:#{#u.nickname},:#{#u.grade.toString()})", nativeQuery = true)
+    fun save(@Param("u") user: User)
 }
